a93c7a8a22817a6c5a05fedf1145e7066b205e4e
niuus <30782922+niuus@users.noreply.github.com>
2018-12-24 20:39:43 -0400

    Upload files part 2

---
+++ source/networkop.cpp
@@ -0,0 +1,258 @@
+/****************************************************************************
+ * Snes9x Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2008-2010
+ *
+ * networkop.cpp
+ *
+ * Network and SMB support routines
+ ****************************************************************************/
+
+#include <errno.h>
+#include <network.h>
+#include <malloc.h>
+#include <ogc/lwp_watchdog.h>
+#include <smb.h>
+
+#include "snes9xgx.h"
+#include "menu.h"
+#include "fileop.h"
+#include "filebrowser.h"
+
+static bool networkInit = false;
+static bool networkShareInit = false;
+char wiiIP[16] = { 0 };
+
+#ifdef HW_RVL
+static int netHalt = 0;
+
+/****************************************************************************
+ * InitializeNetwork
+ * Initializes the Wii/GameCube network interface
+ ***************************************************************************/
+
+static lwp_t networkthread = LWP_THREAD_NULL;
+static u8 netstack[32768] ATTRIBUTE_ALIGN (32);
+
+static void * netcb (void *arg)
+{
+	s32 res=-1;
+	int retry;
+	int wait;
+	static bool prevInit = false;
+
+	while(netHalt != 2)
+	{
+		retry = 5;
+		
+		while (retry>0 && (netHalt != 2))
+		{			
+			if(prevInit) 
+			{
+				int i;
+				net_deinit();
+				for(i=0; i < 400 && (netHalt != 2); i++) // 10 seconds to try to reset
+				{
+					res = net_get_status();
+					if(res != -EBUSY) // trying to init net so we can't kill the net
+					{
+						usleep(2000);
+						net_wc24cleanup(); //kill the net 
+						prevInit=false; // net_wc24cleanup is called only once
+						usleep(20000);
+						break;					
+					}
+					usleep(20000);
+				}
+			}
+
+			usleep(2000);
+			res = net_init_async(NULL, NULL);
+
+			if(res != 0)
+			{
+				sleep(1);
+				retry--;
+				continue;
+			}
+
+			res = net_get_status();
+			wait = 400; // only wait 8 sec
+			while (res == -EBUSY && wait > 0  && (netHalt != 2))
+			{
+				usleep(20000);
+				res = net_get_status();
+				wait--;
+			}
+
+			if(res==0) break;
+			retry--;
+			usleep(2000);
+		}
+		if (res == 0)
+		{
+			struct in_addr hostip;
+			hostip.s_addr = net_gethostip();
+			if (hostip.s_addr)
+			{
+				strcpy(wiiIP, inet_ntoa(hostip));
+				networkInit = true;	
+				prevInit = true;
+			}
+		}
+		if(netHalt != 2) LWP_SuspendThread(networkthread);
+	}
+	return NULL;
+}
+
+/****************************************************************************
+ * StartNetworkThread
+ *
+ * Signals the network thread to resume, or creates a new thread
+ ***************************************************************************/
+void StartNetworkThread()
+{
+	netHalt = 0;
+
+	if(networkthread == LWP_THREAD_NULL)
+		LWP_CreateThread(&networkthread, netcb, NULL, netstack, 8192, 40);
+	else
+		LWP_ResumeThread(networkthread);
+}
+
+/****************************************************************************
+ * StopNetworkThread
+ *
+ * Signals the network thread to stop
+ ***************************************************************************/
+void StopNetworkThread()
+{
+	if(networkthread == LWP_THREAD_NULL || !LWP_ThreadIsSuspended(networkthread))
+		return;
+
+	netHalt = 2;
+	LWP_ResumeThread(networkthread);
+
+	// wait for thread to finish
+	LWP_JoinThread(networkthread, NULL);
+	networkthread = LWP_THREAD_NULL;
+}
+
+#endif
+
+bool InitializeNetwork(bool silent)
+{
+#ifdef HW_RVL
+	StopNetworkThread();
+
+	if(networkInit && net_gethostip() > 0)
+		return true;
+
+	networkInit = false;
+#else
+	if(networkInit)
+		return true;
+#endif
+
+	int retry = 1;
+
+	while(retry)
+	{
+		ShowAction("Initializing network...");
+
+#ifdef HW_RVL
+		u64 start = gettime();
+		StartNetworkThread();
+
+		while (!LWP_ThreadIsSuspended(networkthread))
+		{
+			usleep(50 * 1000);
+
+			if(diff_sec(start, gettime()) > 10) // wait for 10 seconds max for net init
+				break;
+		}
+#else
+		networkInit = !(if_config(wiiIP, NULL, NULL, true, 10) < 0);
+#endif
+
+		CancelAction();
+
+		if(networkInit || silent)
+			break;
+
+		retry = ErrorPromptRetry("Unable to initialize network!");
+		
+#ifdef HW_RVL  	
+		if(networkInit && net_gethostip() > 0)
+#else
+		if(networkInit)
+#endif
+			return true;
+	}
+	return networkInit;
+}
+
+void CloseShare()
+{
+	if(networkShareInit)
+		smbClose("smb");
+	networkShareInit = false;
+	isMounted[DEVICE_SMB] = false;
+}
+
+/****************************************************************************
+ * Mount SMB Share
+ ****************************************************************************/
+
+bool
+ConnectShare (bool silent)
+{
+	if(!InitializeNetwork(silent))
+		return false;
+
+	if(networkShareInit)
+		return true;
+
+	int retry = 1;
+	int chkS = (strlen(GCSettings.smbshare) > 0) ? 0:1;
+	int chkI = (strlen(GCSettings.smbip) > 0) ? 0:1;
+
+	// check that all parameters have been set
+	if(chkS + chkI > 0)
+	{
+		if(!silent)
+		{
+			char msg[50];
+			char msg2[100];
+			if(chkS + chkI > 1) // more than one thing is wrong
+				sprintf(msg, "Check settings.xml.");
+			else if(chkS)
+				sprintf(msg, "Share name is blank.");
+			else if(chkI)
+				sprintf(msg, "Share IP is blank.");
+
+			sprintf(msg2, "Invalid network settings - %s", msg);
+			ErrorPrompt(msg2);
+		}
+		return false;
+	}
+
+	while(retry)
+	{
+		if(!silent)
+			ShowAction ("Connecting to network share...");
+		
+		if(smbInit(GCSettings.smbuser, GCSettings.smbpwd, GCSettings.smbshare, GCSettings.smbip))
+			networkShareInit = true;
+
+		if(networkShareInit || silent)
+			break;
+
+		retry = ErrorPromptRetry("Failed to connect to network share.");
+	}
+
+	if(!silent)
+		CancelAction();
+
+	return networkShareInit;
+}
---
+++ source/networkop.h
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * Snes9x Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2008-2010
+ *
+ * networkop.h
+ *
+ * Network and SMB support routines
+ ****************************************************************************/
+
+#ifndef _NETWORKOP_H_
+#define _NETWORKOP_H_
+
+bool ConnectShare (bool silent);
+void CloseShare();
+
+#endif
