52d4ed5bd89c0af7d72fc1e4660b6b7129b74343
niuus <30782922+niuus@users.noreply.github.com>
2019-01-08 00:05:17 -0400

    Correct the default aspect ratio if the Wii has 16:9 set up [Tantric]
    This sets "Scaling" to "16:9 Correction" by default when first run on the Wii, if the console is set up to widescreen in the "Wii Settings > Screen > Widescreen Settings > Widescreen (16:9)".

--- source/preferences.cpp
+++ source/preferences.cpp
@@ -423,7 +423,13 @@ DefaultSettings ()
 	GCSettings.render = 2; // Unfiltered
 	GCSettings.FilterMethod = FILTER_NONE;	// no hq2x
 
-	GCSettings.widescreen = 0; // no aspect ratio correction
+	GCSettings.widescreen = 0;
+
+#ifdef HW_RVL // correct the default aspect ratio if the Wii has 16:9 set up
+	if (CONF_GetAspectRatio() == CONF_ASPECT_16_9)
+		GCSettings.widescreen = 1;
+#endif
+
 	GCSettings.zoomHor = 1.0; // horizontal zoom level
 	GCSettings.zoomVert = 1.0; // vertical zoom level
 	GCSettings.xshift = 0; // horizontal video shift
