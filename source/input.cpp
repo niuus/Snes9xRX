2ed36afe20c2aa92c73f6e31ff3caa234d7126b2
niuus <30782922+niuus@users.noreply.github.com>
2019-08-05 01:56:38 -0400

    Avoid USB call if there's no Retrode [revvv]

--- source/input.cpp
+++ source/input.cpp
@@ -234,7 +234,7 @@ void ResetControls(int consoleCtrl, int wiiCtrl)
  ***************************************************************************/
 
 #ifdef HW_RVL
-static bool retrodeConnected = false;
+static bool retrodeSetup = false;
 static s32 deviceIdRetrode = 0;
 static u8 endpointRetrode = 0;
 static u8 bMaxPacketSizeRetrode = 0;
@@ -337,10 +337,10 @@ UpdatePads()
 	#endif
 
 	#ifdef HW_RVL
-	if(!retrodeConnected)
+	if(!retrodeSetup)
 	{
 		openRetrode();
-		retrodeConnected = true;
+		retrodeSetup = true;
 	}
 	#endif
 
@@ -502,7 +502,7 @@ static void decodepad (int chan)
 
 #ifdef HW_RVL
 	// Retrode
-	if (retrodeConnected && chan == 0)
+	if (deviceIdRetrode != 0 && chan == 0)
 	{
 	    uint8_t ATTRIBUTE_ALIGN(32) buf[bMaxPacketSizeRetrode];
 		int res = USB_ReadIntrMsg(deviceIdRetrode, endpointRetrode, sizeof(buf), buf);
@@ -538,7 +538,7 @@ static void decodepad (int chan)
 			jpRetrode[buf[0] - 1] = jp12;
 		}
 	}
-	if (retrodeConnected && chan >= 0 && chan <= 3)
+	if (deviceIdRetrode != 0 && chan >= 0 && chan <= 3)
 	{
 		jp |= jpRetrode[chan]; // restore previous value
 	}
