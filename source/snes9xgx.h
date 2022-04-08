2cffb79609140f5564be07423d0068c10e5d48e1
niuus <30782922+niuus@users.noreply.github.com>
2022-04-07 22:18:59 -0400

    Load cover images directly from file
    ...instead of an intermediary buffer. [Daryl]

--- source/snes9xgx.h
+++ source/snes9xgx.h
@@ -20,7 +20,7 @@
 #include "filelist.h"
 
 #define APPNAME 			"Snes9x RX"
-#define APPVERSION 			"4.9.0"
+#define APPVERSION 			"5.0.0"
 #define APPFOLDER 			"snes9xgx"
 #define PREF_FILE_NAME		"settingsRX.xml"
 
@@ -126,8 +126,6 @@ struct SGCSettings{
 
 	int		Interpolation;
 };
-
-char* ImageFolder();
 
 void ExitApp();
 void ShutdownWii();
