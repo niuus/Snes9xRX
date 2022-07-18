378aef3c6936ba6bcfd40da9f1776b7819fc3b71
niuus <30782922+niuus@users.noreply.github.com>
2022-07-18 12:50:45 -0400

    Version bump

--- source/snes9xgx.h
+++ source/snes9xgx.h
@@ -60,7 +60,7 @@ enum
 };
 
 const char ctrlName[6][24] =
-{ "SNES Controller", "SNES Mouse", "Super Scope", "Justifier", "SNES Controllers (2)", "SNES Controllers (4)" };
+{ "SNES Controller", "SNES Mouse", "Superscope", "Justifier", "SNES Controllers (2)", "SNES Controllers (4)" };
 
 enum {
 	TURBO_BUTTON_RSTICK = 0,
@@ -126,11 +126,9 @@ struct SGCSettings{
 	int		render;		// 0 - original, 1 - filtered, 2 - unfiltered
 	int		FilterMethod; // convert to RenderFilter
 	int		Controller;
-	int		HiResolution;
-	int		SpriteLimit;
+	int		crosshair;
 	int		ShowFrameRate;
 	int		ShowLocalTime;
-	int		crosshair;
 	int		widescreen;	// 0 - 4:3 aspect, 1 - 16:9 aspect
 	int		xshift;	// video output shift
 	int		yshift;
@@ -143,7 +141,6 @@ struct SGCSettings{
 	int		PreviewImage;
 
 	int		sfxOverclock;
-	int		cpuOverclock;
 
 	int		ReverseStereo;
 	int		Interpolation;
